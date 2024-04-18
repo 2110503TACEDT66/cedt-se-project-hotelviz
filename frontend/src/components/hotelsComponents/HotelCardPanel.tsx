02ed03210557abe1eac1a4f901a6a9bff4a03065
Pannavich Tanyavichitkul <pannavich.tan@gmail.com>
2024-04-18 14:29:15 +0700

    UI sort final

--- frontend/src/components/hotelsComponents/HotelCardPanel.tsx
+++ frontend/src/components/hotelsComponents/HotelCardPanel.tsx
@@ -270,7 +270,7 @@ export default function HotelCardPanel({ session = null }: { session?: any }) {
               Rating :
               <div className="flex ml-2 mt-1">
               <div className="mt-1 mr-1 text-lg text-neutral-400">≥</div>
-              <Rating size='large' onChange={(e,newValue) => {
+              <Rating size='large' precision={0.5} onChange={(e,newValue) => {
                   e.stopPropagation; 
                   if(newValue){setRating(newValue);}
                   else{setRating(0);}
