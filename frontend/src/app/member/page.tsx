7613b503fc6e679cc15c6ba686aa13dfd7fac03c
SureepornChayanopparat <6633270321@student.chula.ac.th>
2024-04-25 12:49:24 +0700

    member infomation box

--- frontend/src/app/member/page.tsx
+++ frontend/src/app/member/page.tsx
@@ -1,9 +1,14 @@
 import MemberExp from "@/components/memberComponents/MemberExp";
+import MemberInfo from "@/components/memberComponents/MemberInfo";
 
 export default async function Mybookings() {
     return(
         <main>
-          <MemberExp/>
+          <div className="flex">
+            <MemberExp/>
+            <MemberInfo/>
+          </div>
+          
         </main>
     )
 }
---
+++ frontend/src/components/memberComponents/MemberInfo.tsx
@@ -0,0 +1,22 @@
+
+
+export default function MemberInfo() {
+    return(
+        <a className="m-2 text-base border border-gray-300 w-full h-[200px] rounded-xl bg-white p-3">
+            <table>
+                <tr>
+                    <td className="w-[65px]">Name</td>
+                    <td>name</td>
+                </tr>
+                <tr>
+                    <td>Tel</td>
+                    <td>012-4567890</td>
+                </tr>
+                <tr>
+                    <td>Email</td>
+                    <td>123@email.com</td>
+                </tr>
+            </table>
+        </a>
+    )
+}
