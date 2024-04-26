ab0b201612295e8bd08bffb5a43479accf0c1f4d
Pannavich Tanyavichitkul <pannavich.tan@gmail.com>
2024-04-26 15:43:50 +0700

    memberexp

--- frontend/src/components/memberComponents/MemberExp.tsx
+++ frontend/src/components/memberComponents/MemberExp.tsx
@@ -6,7 +6,7 @@ import BedtimeIcon from '@mui/icons-material/Bedtime';
 export default function MemberExp() {
 
     return (
-        <div className="flex m-2 p-1 border border-gray-300 w-[800px] h-[200px] rounded-xl bg-white ">
+        <div className="flex m-2 pl-3  border border-gray-300 w-[800px] h-[200px] rounded-xl bg-white ">
         <div className='relative ml-1' >
             <Gauge value={75} valueMax={200} 
             startAngle={-120} endAngle={120} width={180}
